c1ec51020912afc12c619f5d9f9876a3e9cbf9e6
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-04-25 08:47:10 +0530

    working on oop

--- oopBasic.java
+++ oopBasic.java
@@ -1,12 +1,19 @@
+// modifier -> 4
+// default -> whene nothing is specify the code is access only on same pakage
+// public -> can access by any class 
+// private -> only access within same class
+// protected -> code is access within same pakage and subclass
 
 class Student {
     int rank;
     int age;
     String name;
+    static String gender;
+    private int pass; // can only access within this class
 
     // public method can only access from object
     public void info() {
-        System.out.println("The " + name + "is " + age + " year old. with rank of " + rank);
+        System.out.println("The " + name + "is " + age + " year old " + gender + " with rank of " + rank);
     }
 
     // static method can access directly from class
@@ -15,24 +22,54 @@ class Student {
     }
 
     // constructor in java
-    Student(String name) {
+    Student(String name, int pass) {
         this.name = name;
+        this.pass = pass;
         System.out.println(" ==> The student constructor has been executed. <==");
+        System.out.println(pass);
     }
 
+}
+
+// abstract and final keyword
+
+// no class can inherite teacher class
+final class teacher {
+    String name;
+    int age;
+
+    public void info() {
+        System.out.println("This is teacher class . ");
+    }
+}
+
+// we cannot create object from School class
+// we can only inherite from school class
+abstract class School {
+    public void info() {
+        System.out.println("This is school class .");
+    }
 }
 
 public class oopBasic {
     public static void main(String args[]) {
 
         // creating object from student class
-        Student s1 = new Student("vaibhav");
+        Student s1 = new Student("vaibhav", 124);
         s1.rank = 1;
         s1.age = 20;
         s1.info();
 
         // accessing static class
         Student.classInfo(); // static method can be access directly without creating object
+        Student.gender = "male";
+        s1.info();
+
+        // abstract - try to access school class
+        // School alpha = new School(); // this is not valid
+
+        // final - can be use to create object
+        // teacher thorfin = new teacher();
 
     }
 }
