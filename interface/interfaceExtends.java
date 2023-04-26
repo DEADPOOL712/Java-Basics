1a6f0c125bd37f4db53ac8c8919477518e693cee
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-04-26 15:33:14 +0530

    interface extends

---
+++ interface/interfaceExtends.java
@@ -0,0 +1,43 @@
+import javax.swing.undo.StateEdit;
+
+interface p {
+    public int p = 10;
+
+    public void method_p();
+}
+
+interface p1 extends p {
+    public int p1 = 20;
+}
+
+interface p2 extends p {
+    public int p3 = 30;
+}
+
+interface p12 extends p1, p2 {
+    public void method_p12();
+}
+
+// creating class from interface p12
+class Q implements p12 {
+    public void method_p() {
+        System.out.println("This method from interface p");
+    }
+
+    public void method_p12() {
+        System.out.println("This method from interface p12");
+    }
+}
+
+public class interfaceExtends {
+
+    public static void main(String args[]) {
+
+        // creating obj from class Q
+        Q objQ = new Q();
+        objQ.method_p();
+        objQ.method_p12();
+        System.out.println(objQ.p3);
+
+    }
+}
