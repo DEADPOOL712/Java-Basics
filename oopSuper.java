0ae6ef6ee9616f7c90423990e3fe9e40b612fa4e
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-04-27 09:40:09 +0530

    super key

---
+++ oopSuper.java
@@ -0,0 +1,36 @@
+class Website {
+    String name;
+    int traffic;
+    String type;
+
+    public void info() {
+        System.out.printf("website:%s type:%s traffic:%d", name, type, traffic);
+
+    }
+
+    Website(String n, int t, String type) {
+        this.name = n;
+        this.traffic = t;
+        this.type = type;
+
+    }
+}
+
+class Myweb extends Website {
+    String name = "lol"; // this keyword refer to instant of that class
+    // while super keyword refer to the instant of sueprclass
+
+    Myweb(String name, int traffic) {
+        super(name, traffic, "e-commerce");
+        System.out.println(this.name);
+        System.out.println(super.name);
+        super.info();
+    }
+}
+
+public class oopSuper {
+    public static void main(String args[]) {
+        Myweb v1 = new Myweb("Oxyton", 200);
+        // System.out.println("Hello !");
+    }
+}
